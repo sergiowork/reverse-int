13d7613f567634c53d79f0d5184d0c63300604e5
sergiowork <sergioworkit@gmail.com>
2020-03-22 14:46:55 +0200

    fix: comeback to previous version function reverse (n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-    return parseFloat(n.toString().split('').reverse().join(''));
+    let str = String(n);
+    let arr = str.split('');
+    let reversArray = [];
+
+    for(let i = str.length-1; i >= 0; i--) {
+        let aaa = arr[i];
+        reversArray.push(arr[i]);
+    }
+
+    return parseInt(reversArray.join(''));
 }
